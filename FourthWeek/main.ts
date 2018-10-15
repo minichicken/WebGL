1e2a386da53643c63eff037c8e6162d42c994d7b
Jaeho Lee <mini.chicken.dev@gmail.com>
2018-10-15 17:28:56 +0900

    apply threejs

--- FourthWeek/main.ts
+++ FourthWeek/main.ts
@@ -1,1 +1,35 @@
-console.log('dd');
+import { PerspectiveCamera, Scene, WebGLRenderer, Mesh, TextureLoader, BoxBufferGeometry, MeshBasicMaterial } from 'three';
+
+let camera: PerspectiveCamera | any, scene: Scene, renderer: WebGLRenderer;
+let mesh: Mesh;
+
+function init() {
+    camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 400;
+    scene = new Scene();
+    var texture = new TextureLoader().load('crate.gif');
+    var geometry = new BoxBufferGeometry(200, 200, 200);
+    var material = new MeshBasicMaterial({ map: texture });
+    mesh = new Mesh(geometry, material);
+    scene.add(mesh);
+    renderer = new WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    //
+    window.addEventListener('resize', onWindowResize, false);
+}
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+function animate() {
+    requestAnimationFrame(animate);
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+init();
+animate();
